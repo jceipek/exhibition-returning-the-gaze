85c92ce2bd9ace043f7502bfd1bbf663abc90670
behnaz <farahibo@gmail.com>
2020-08-04 16:35:25 -0700

    deploy

--- deploy.js
+++ deploy.js
@@ -1,9 +1,58 @@
-const ghpages = require('gh-pages');
+const simpleGit = require('simple-git');
+const fs = require('fs-extra');
+const path = require('path');
 
-ghpages.publish('dist', function(err) {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log("Deployed");
+
+const options = {
+    baseDir: process.cwd(),
+    binary: 'git',
+    maxConcurrentProcesses: 6,
+};
+
+const git = simpleGit(options);
+
+const directory = '.';
+let distFolder = path.join(directory, "dist");
+fs.readdir(directory).then((files) => {
+    let deleteAllExceptDist = [];
+    for (const file of files) {
+        if (file != ".git" &&
+            file != ".gitignore" &&
+            file != ".vscode" &&
+            file != "dist") {
+            console.log("Delete", file);
+            deleteAllExceptDist.push(fs.unlink(path.join(directory, file)));
+        }
     }
+    return Promise.all(deleteAllExceptDist);
+}).then(() => {
+    console.log("Checkout gh-pages");
+    return git.checkout("gh-pages");
+}).then(() => {
+    console.log("Delete dist folder");
+    return fs.readdir(distFolder);
+}).then((files) => {
+    console.log("Move all files out");
+    let moveUp = files.map((file) => { return fs.move(path.join(distFolder, file), path.join(directory, file)); });
+    return Promise.all(moveUp);
+}).then(() => {
+    // delete dist
+    console.log("Delete dist folder");
+    return fs.unlink(distFolder);
+}).then(() => {
+    // git add everything
+    console.log("git add everything");
+    return git.add(".");
+}).then(() => {
+    // git commit
+    console.log("Commit");
+    return git.commit("Deploy");
+}).then(() => {
+    // git push
+    console.log("Push");
+    return git.push();
+}).then(() => {
+    console.log("Checkout working branch");
+    // git checkout all again
+    return git.checkout("AllRooms/all");
 });
