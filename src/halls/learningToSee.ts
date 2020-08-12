e4df65cc94d98cd581cf81166b72f49cfcff4cad
Memo Akten <memo@memo.tv>
2020-08-12 16:59:21 +0300

    first version of dynamic plane settings and renderer

--- src/halls/learningToSee.ts
+++ src/halls/learningToSee.ts
@@ -1,4 +1,4 @@
-import { Scene, PerspectiveCamera, PlaneGeometry, MeshBasicMaterial, Mesh, WebGLRenderer, VideoTexture, LinearFilter, RGBFormat } from "three";
+import { Scene, PerspectiveCamera, PlaneGeometry, MeshBasicMaterial, Mesh, WebGLRenderer, VideoTexture, LinearFilter, RGBFormat, MathUtils, Object3D, Vector3 } from "three";
 import { normalizeWheel } from "../utils"
 import { Halls, Hall, HallState } from "../common"
 
@@ -10,11 +10,12 @@ import iconPath from "../media/map/learningtosee.png";
 interface LearningToSeeHall extends Hall {
 
     state: {
+        settings: any,
         videoSrcs: string[],
-        planeData: { pos: [number, number, number], rot: [number, number, number] }[],
         vids: HTMLVideoElement[],
         scene: Scene,
         camera: PerspectiveCamera,
+        planeGroups: Mesh[][],
         progressFrac: number,
         loadedOnce: boolean
     }
@@ -25,93 +26,79 @@ const thisHall: LearningToSeeHall = {
     iconPath,
     introId: "js-learning-to-see-hall",
     state: {
+        settings: {
+            startDistance: 5, // distance to first set of planes
+            depthSpacing: 8, // distance in depth between planeGroups
+            widthSpacingNear: 1, // minumum width between adjacent planes
+            widthSpacingCamZMult: 0.003, // how camera distance affects width spacing
+            widthSpacingCamZPow: 3, // order of camera distance width spacing influence
+            rotMult: 2, // how much to multiply current rotation by (after looking at camera)
+            moveSpeed: 0.1, // how fast each plane moves to target
+            scrollSpeed: 0.0001, // how fast scrolling affects movement
+        },
         videoSrcs: [],
-        planeData: [],
         vids: [],
         scene: new Scene(),
-        camera: new PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000),
+        camera: new PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 100),
+        planeGroups: [],
         progressFrac: 0,
         loadedOnce: false
     },
     setup: async function (): Promise<void> {
-        function postLoad () {
-            thisHall.state.progressFrac= 0;
+        function postLoad() {
+            thisHall.state.progressFrac = 0;
             thisHall.state.camera.position.set(0, 0, 0);
         }
         return new Promise<void>((resolve) => {
             if (!thisHall.state.loadedOnce) {
                 let state = thisHall.state;
-                // state.videoSrcs = [
-                //     video1src,
-                //     video2src,
-                //     video3src,
-                //     video4src,
-                //     video5src,
-                //     video6src,
-                // ];
                 state.videoSrcs = [
                     videoDual1src,
                     videoDual2src,
                     videoDual3src,
                 ];
-                state.planeData = [
-                    { pos: [-.55, 0, 3 - 5], rot: [0, 35, 0] },
-                    { pos: [.55, 0, 3 - 5], rot: [0, -35, 0] },
-                    { pos: [-0.7, 0, 5 - 9], rot: [0, 35, 0] },
-                    { pos: [0.7, 0, 5 - 9], rot: [0, -35, 0] },
-                    { pos: [-1.0, 0, 5 - 11], rot: [0, 35, 0] },
-                    { pos: [1.0, 0, 5 - 11], rot: [0, -35, 0] },
-                ];
-                
+
                 Promise.all(state.videoSrcs.map(makeVideo)).then((videos) => {
                     state.vids = videos;
 
                     let materials = state.vids.map(makeMaterial);
-                    for (let i = 0; i < state.planeData.length; i+=2) {
-                        let material = materials[Math.floor(i/2)];
-                        let yMin = 59.8/716.1;
-                        let yMax = 652.4/716.1;
-                        {
+                    for (let i = 0; i < state.vids.length; i++) {
+                        let vid = state.vids[i];
+                        let numPanels = Math.round(vid.width / vid.height); // should be 1 or 2
+                        console.log(vid, 'numPanels:', numPanels)
+                        // console.log(vid.width, vid.height);
+
+                        let material = materials[i];
+                        let yMin = 0
+                        let yMax = 1;
+                        let planeGroup = [];
+
+                        for (let panel = 0; panel < numPanels; panel++) {
                             let geometry = new PlaneGeometry(1, 1, 1);
                             let uvs = geometry.faceVertexUvs[0];
-                            let xMin = 28.4/1269.2;
-                            let xMax = 600/1269.2;
-                            
-                            uvs[0][0].set( xMin , yMax );
-                            uvs[0][1].set( xMin , yMin );
-                            uvs[0][2].set( xMax , yMax );
-                            uvs[1][0].set( xMin , yMin );
-                            uvs[1][1].set( xMax , yMin );
-                            uvs[1][2].set( xMax , yMax );
-                        
+                            let xMin = panel * 1 / numPanels;
+                            let xMax = xMin + 1 / numPanels;
+
+                            uvs[0][0].set(xMin, yMax);
+                            uvs[0][1].set(xMin, yMin);
+                            uvs[0][2].set(xMax, yMax);
+                            uvs[1][0].set(xMin, yMin);
+                            uvs[1][1].set(xMax, yMin);
+                            uvs[1][2].set(xMax, yMax);
+
                             let plane = new Mesh(geometry, material);
-                            let pos = state.planeData[i].pos;
-                            let rot = state.planeData[i].rot;
-                            plane.position.set(pos[0], pos[1], pos[2]);
-                            plane.rotation.set(0, rot[1] * 2 * Math.PI / 360, 0);
                             state.scene.add(plane);
+                            planeGroup.push(plane);
+
+                            let bgPlane = new Mesh(geometry, new MeshBasicMaterial());
+                            let ss = 1.05;
+                            bgPlane.scale.set(ss, ss, ss);
+                            bgPlane.position.set(0, 0, -0.01);
+                            plane.add(bgPlane);
                         }
-                        {
-                            let geometry = new PlaneGeometry(1, 1, 1);
-                            let uvs = geometry.faceVertexUvs[0];
-                            let xMin = 667.1/1269.2;
-                            let xMax = 1290/1269.2;
-                            uvs[0][0].set( xMin , yMax );
-                            uvs[0][1].set( xMin , yMin );
-                            uvs[0][2].set( xMax , yMax );
-                            uvs[1][0].set( xMin , yMin );
-                            uvs[1][1].set( xMax , yMin );
-                            uvs[1][2].set( xMax , yMax );
-                        
-                            let plane = new Mesh(geometry, material);
-                            let pos = state.planeData[i+1].pos;
-                            let rot = state.planeData[i+1].rot;
-                            plane.position.set(pos[0], pos[1], pos[2]);
-                            plane.rotation.set(0, rot[1] * 2 * Math.PI / 360, 0);
-                            state.scene.add(plane);
-                        }
+
+                        state.planeGroups.push(planeGroup);
                     }
-                    // plane.rotation.set(Math.PI/3,Math.PI/3,Math.PI/3);
                     thisHall.state.loadedOnce = true;
                     postLoad();
                     resolve();
@@ -139,7 +126,58 @@ const thisHall: LearningToSeeHall = {
         removeEventListeners();
     },
     render: function (renderer) {
-        renderer.render(thisHall.state.scene, thisHall.state.camera);
+        let state = thisHall.state;
+        let settings = state.settings;
+        let cam = state.camera;
+
+        for (let planeGroupIdx = 0; planeGroupIdx < state.planeGroups.length; planeGroupIdx++) {
+            let planeGroup = state.planeGroups[planeGroupIdx];
+
+            for (let planeIdx = 0; planeIdx < planeGroup.length; planeIdx++) {
+                let plane = planeGroup[planeIdx];
+                
+                // set plane position
+                let camZDist = Math.abs(plane.position.z - cam.position.z);
+                let xposMult = 0;
+                let yposMult = 0;
+                if(planeGroup.length == 1) {
+                    xposMult = 0;
+                    yposMult = 0.9;
+                } else {
+                    xposMult = planeIdx % 2 == 0 ? -1 : 1;
+                    yposMult = 0;
+                }
+                let distOffset = Math.pow(camZDist, settings.widthSpacingCamZPow) * settings.widthSpacingCamZMult;
+                let targetPosition = new Vector3(xposMult * (settings.widthSpacingNear / 2 + distOffset), yposMult * distOffset, -(settings.startDistance + settings.depthSpacing * planeGroupIdx));
+
+                plane.position.addScaledVector(targetPosition.sub(plane.position), settings.moveSpeed);
+
+                // set plane orientation
+                plane.lookAt(cam.position);
+                plane.rotation.set(0, plane.rotation.y * settings.rotMult, 0);
+
+                // set volume of videos based on camera distance
+                if (planeIdx == 0) { // only do it for first plane in planeGroup
+                    let volume;
+                    if (cam.position.z < plane.position.z) { // if screens are behind camera, quicker fade out
+                        volume = MathUtils.clamp(1 - (2 * camZDist / settings.depthSpacing), 0, 1);
+                        volume *= volume * volume * volume;
+                    } else {
+                        volume = MathUtils.clamp(camZDist / settings.depthSpacing, 0, 1);
+                        // volume = volume * volume;
+                        volume = 1 - volume;
+                    }
+                    state.vids[planeGroupIdx].volume = volume;
+                }
+            }
+        }
+
+        // update camera
+        let length = settings.startDistance + settings.depthSpacing * state.videoSrcs.length;
+        let targetCamZ = -state.progressFrac * length;
+        cam.position.set(0, 0, (targetCamZ - cam.position.z) * settings.moveSpeed + cam.position.z);
+
+        renderer.render(state.scene, cam);
     },
 
     resize: function () {
@@ -166,15 +204,14 @@ const windowEventListeners: WindowListeners = {
 
     wheel: (scrollEvt: WheelEvent) => {
         let evt = normalizeWheel(scrollEvt);
-        let length = 6.5;
-        thisHall.state.progressFrac -= evt.pixelY * 0.0001 * length;
-        thisHall.state.progressFrac = Math.max(0, Math.min(1, thisHall.state.progressFrac));
-        thisHall.state.camera.position.set(0, 0, thisHall.state.progressFrac * -length);
+        thisHall.state.progressFrac -= evt.pixelY * thisHall.state.settings.scrollSpeed;
+        thisHall.state.progressFrac = MathUtils.clamp(thisHall.state.progressFrac, 0, 1);
+        // console.log('scrollEvt', thisHall.state.progressFrac);
     },
 
     mousemove: (evt: MouseEvent) => {
         let frac = (evt.clientX - window.innerWidth / 2) / (window.innerWidth / 2); // [-1..1]
-        thisHall.state.camera.rotation.set(0, -frac * 0.3, 0);
+        thisHall.state.camera.rotation.set(0, -frac * 0.5, 0);
     },
 }
 
