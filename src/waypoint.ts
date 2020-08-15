c52513ba4e85ba2d03bed3c68ced058b4d536fce
Memo Akten <memo@memo.tv>
2020-08-15 13:22:41 +0300

    fixes to waypoint.ts to allow better control of waypoint placement from app

--- src/waypoint.ts
+++ src/waypoint.ts
@@ -49,21 +49,22 @@ export function waypointUpdate(waypoint: WaypointState, currProgress: number) : 
     return currProgress;
 }
 
-export function waypointMoveToMouse(mouseNDC : { x: number, y: number }, waypoint: WaypointState, camera: Camera, maxZ: number, outWaypointPos: Vector3) {
+export function waypointMoveToMouse(mouseNDC : { x: number, y: number }, waypoint: WaypointState, camera: Camera, maxZ: number, outWaypointPos: Vector3, mouseMinY:number = -0.5) {
     if (waypoint.state === WaypointMovingState.Idle) {
         document.body.style.cursor = "pointer";
     } else {
         document.body.style.cursor = "";
     }
-    waypoint.lastMouseNorm.set(mouseNDC.x, Math.min(mouseNDC.y, -.5));
+    waypoint.lastMouseNorm.set(mouseNDC.x, Math.min(mouseNDC.y, mouseMinY));
     waypoint.raycaster.setFromCamera(waypoint.lastMouseNorm, camera);
     waypoint.raycaster.ray.intersectPlane(waypoint.groundPlane, /* out */ outWaypointPos);
 
     // Limit the maximum movement you can take at once so you can't leave a hall with one click
     let signedDist = outWaypointPos.z - camera.position.z;
-    let moveCap = maxZ * 0.9;
+    let moveCap = maxZ;// * 0.9; // The 0.9 is interfering with my maxZ Calulations :) /Memo
     if (Math.abs(signedDist) > moveCap) {
-        outWaypointPos.z = camera.position.z + Math.sign(signedDist)*moveCap;
+        let wz = camera.position.z + Math.sign(signedDist)*moveCap;
+        outWaypointPos.multiplyScalar(wz / outWaypointPos.z); // scale entire vector
     }
 }
 
